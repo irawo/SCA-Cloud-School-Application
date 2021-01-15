2158c5a0c22e5f6bff3334ed3b37f974a3d66f8e
Ubuntu <ubuntu@ip-172-31-6-49.us-west-2.compute.internal>
2021-01-15 18:11:13 +0000

    initial commit.

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Welcome to SCA Cloud School Application');
+}).listen(8080); 
