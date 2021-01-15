9185c61240d71368d8b70977779be665cb326b5b
Ajayi Elizabeth Olasumbo <sumbic@yahoo.com>
2021-01-15 23:44:10 +0100

    added assement messge to feature branch

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to SCA Cloud School Application');
+  res.end('Welcome to SCA Cloud School Application , this is my first assessment');
 }).listen(8080); 
